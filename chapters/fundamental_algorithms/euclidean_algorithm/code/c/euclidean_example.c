0028e5e80b008e09c22d57e65c1337f47b0cdfab
Gustorn <gustorn@gmail.com>
2018-03-07 20:50:07 +0100

    Apply clang-format style to C code as well

--- chapters/fundamental_algorithms/euclidean_algorithm/code/c/euclidean_example.c
+++ chapters/fundamental_algorithms/euclidean_algorithm/code/c/euclidean_example.c
@@ -1,43 +1,38 @@
-#include <stdio.h>
 #include <math.h>
+#include <stdio.h>
 
-int euclid_mod(int a, int b)
-{
-    a = abs(a);
-    b = abs(b);
+int euclid_mod(int a, int b) {
+  a = abs(a);
+  b = abs(b);
 
-    while (b != 0){
-        int temp = b;
-        b = a%b;
-        a = temp;
+  while (b != 0) {
+    int temp = b;
+    b = a % b;
+    a = temp;
+  }
+
+  return a;
+}
+
+int euclid_sub(int a, int b) {
+  a = abs(a);
+  b = abs(b);
+
+  while (a != b) {
+    if (a > b) {
+      a -= b;
+    } else {
+      b -= a;
     }
+  }
 
-    return a;
+  return a;
 }
 
-int euclid_sub(int a, int b)
-{
-    a = abs(a);
-    b = abs(b);
+int main() {
+  int check1 = euclid_mod(64 * 67, 64 * 81);
+  int check2 = euclid_sub(128 * 12, 128 * 77);
 
-    while (a != b) {
-        if (a > b) {
-            a -= b;
-        }
-        else {
-            b -= a;
-        }
-    }
-
-    return a;
+  printf("%d\n", check1);
+  printf("%d\n", check2);
 }
-
-int main()
-{
-    int check1 = euclid_mod(64*67, 64*81);
-    int check2 = euclid_sub(128*12, 128*77);
-
-    printf("%d\n", check1);
-    printf("%d\n", check2);
-}
-
